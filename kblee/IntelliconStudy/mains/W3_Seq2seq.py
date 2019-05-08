fff26f4584aff831a948f90074b622d7146e5120
kangbok <kangbok.kbl@gmail.com>
2019-05-09 00:30:27 +0900

    kblee - 3~4주 seq2seq (결과는 나오지만 특정 단어에만 수렴)

--- kblee/IntelliconStudy/mains/W3_Seq2seq.py
+++ kblee/IntelliconStudy/mains/W3_Seq2seq.py
@@ -12,19 +12,20 @@ with open("../resource/idx_vocab_dict.pkl", "rb") as f:
 
 batch_size = 1
 vocab_size = len(vocab_idx_dict.keys())
-lstm_units = 128
+encoder_lstm_units = 256
+decoder_lstm_units = 256
 
 encoder_x = tf.placeholder(tf.float32, [1, None, vocab_size])
 # decoder_x = tf.placeholder(tf.float32, [1, None, vocab_size])
 decoder_y = tf.placeholder(tf.int32, [1, None, vocab_size])
 
 ##### encoder
-encoder_cell = tf.nn.rnn_cell.LSTMCell(lstm_units)
+encoder_cell = tf.nn.rnn_cell.LSTMCell(encoder_lstm_units)
 encoder_dropout_cell = tf.nn.rnn_cell.DropoutWrapper(encoder_cell, output_keep_prob=0.75)
 encoder_output, encoder_state = tf.nn.dynamic_rnn(encoder_dropout_cell, encoder_x, dtype=tf.float32, scope="encoder_rnn")
 
 ##### decoder functions
-W = tf.Variable(tf.truncated_normal(shape=[lstm_units, vocab_size], stddev=0.1))
+W = tf.Variable(tf.truncated_normal(shape=[decoder_lstm_units, vocab_size], stddev=0.1))
 b = tf.Variable(tf.constant(0.1, shape=[vocab_size]))
 
 def loop_fn(time, cell_output, cell_state, loop_state):
@@ -69,18 +70,21 @@ def loop_fn_transition(time, prev_output, prev_state, prev_loop_state):
 
 
 ##### decoder
-decoder_cell = tf.nn.rnn_cell.LSTMCell(lstm_units)
+decoder_cell = tf.nn.rnn_cell.LSTMCell(decoder_lstm_units)
 decoder_dropout_cell = tf.nn.rnn_cell.DropoutWrapper(decoder_cell, output_keep_prob=0.75)
 decoder_output_ta, decoder_state, decoder_tmp = tf.nn.raw_rnn(decoder_dropout_cell, loop_fn, scope="decoder_rnn")
 decoder_outputs = decoder_output_ta.stack()
 
 ##### 학습 영역
-decoder_max_steps, decoder_batch_size, decoder_dim = tf.unstack(tf.shape(decoder_outputs))
-decoder_outputs_flat = tf.reshape(decoder_outputs, (-1, decoder_dim))
+# final_length = tf.minimum(tf.shape(decoder_y)[1], tf.shape(decoder_outputs)[0])
+filtered_decoder_outputs = tf.gather(decoder_outputs, tf.range(0, tf.shape(decoder_y)[1]))
+decoder_max_steps, decoder_batch_size, decoder_dim = tf.unstack(tf.shape(filtered_decoder_outputs))
+decoder_outputs_flat = tf.reshape(filtered_decoder_outputs, (-1, decoder_dim))
 decoder_logits_flat = tf.add(tf.matmul(decoder_outputs_flat, W), b)
 decoder_logits = tf.reshape(decoder_logits_flat, (decoder_max_steps, decoder_batch_size, vocab_size))
 
-# re_decoder_y = tf.reshape(decoder_y, (-1, vocab_size))
+# filtered_decoder_y = tf.gather(decoder_y, tf.range(0, final_length), axis=1)
+
 cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(labels=decoder_y, logits=decoder_logits)
 loss = tf.reduce_mean(cross_entropy)
 optimizer = tf.train.AdamOptimizer(1e-4).minimize(loss)
@@ -98,10 +102,16 @@ def create_one_hot_matrix(stc_list):
         for word in word_list:
             one_hot_vector = np.zeros((vocab_size, ), dtype=int)
             one_hot_vector[vocab_idx_dict[word]] = 1
-            # one_hot_vector.reshape((1, vocab_size))
+            one_hot_vector.reshape((1, vocab_size))
             out_list.append(one_hot_vector)
 
     return np.array(out_list)
+
+def padding_one_hot():
+    one_hot_vector = np.zeros((vocab_size,), dtype=int)
+    one_hot_vector[2] = 1
+
+    return one_hot_vector
 
 
 def create_encoder_decoder_io(one_hot_matrix):
@@ -113,10 +123,12 @@ def create_encoder_decoder_io(one_hot_matrix):
     end_char_vector[1] = 1
 
     decoder_i = np.append(start_char_vector, one_hot_matrix[1:])
-    decoder_o = np.append(one_hot_matrix[1:], end_char_vector)
+    # decoder_o = np.append(one_hot_matrix[1:], end_char_vector)
+    decoder_o = one_hot_matrix[1:]
 
-    for _ in range(100 - int((decoder_o.shape[0] / vocab_size))):
-        decoder_o = np.append(decoder_o, np.zeros((vocab_size,), dtype=int))
+    # 100으로 사이즈 맞춰줄 때 쓰던 부분
+    # for _ in range(100 - int((decoder_o.shape[0] / vocab_size))):
+    #     decoder_o = np.append(decoder_o, padding_one_hot())
 
     encoder_i = encoder_i.reshape((1, 1, -1))
     decoder_i = decoder_i.reshape((1, -1, vocab_size))
@@ -163,17 +175,18 @@ with tf.Session() as sess:
             continue
 
         encoder_x_, decoder_x_, decoder_y_ = create_encoder_decoder_io(one_hot_matrix)
-        # ccc = sess.run(decoder_outputs, feed_dict={encoder_x: encoder_x_, decoder_y: decoder_y_})
+        # ccc = sess.run(decoder_outputs_flat, feed_dict={encoder_x: encoder_x_, decoder_y: decoder_y_})
         #
         # print("a")
 
-        # 매 1,000개마다 step print, 모델 저장
-        if cnt % 1000 == 0:
+        # 매 10개마다 step print, 모델 저장
+        if cnt % 10 == 0:
             test_result = sess.run(decoder_prediction, feed_dict={encoder_x: encoder_x_, decoder_y: decoder_y_})
             print("step %s" % cnt)
+            print(input_words)
             print_tmp_result(encoder_x_, test_result)
 
-            saver.save(sess, checkpoint_path)
+            # saver.save(sess, checkpoint_path)
 
         sess.run(optimizer, feed_dict={encoder_x: encoder_x_, decoder_y: decoder_y_})
 

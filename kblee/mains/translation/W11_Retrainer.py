5664a49ea7fc74bda6be0a705800d04f11674183
kblee <kblee@intellicon.co.kr>
2019-06-27 21:05:54 +0900

    kblee - retrainer 학습회수 변경

--- kblee/mains/translation/W11_Retrainer.py
+++ kblee/mains/translation/W11_Retrainer.py
@@ -134,7 +134,7 @@ with tf.Session() as sess:
 
     for epoch in range(EPOCH):
         for idx in range(0, len(encoder_corpus), BATCH_SIZE):
-            if cnt > 48900:
+            if cnt > 73900:
                 is_start = True
 
             if not is_start:
